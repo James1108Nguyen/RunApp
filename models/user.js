89f8097fa2d920de4fe7fca535580c6954d78195
Nguyen Ngoc Thinh <nguyenngocthinh@Nguyens-MacBook-Pro.local>
2021-05-28 19:23:42 +0700

    Add login

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+var uniqueValidator = require("mongoose-unique-validator")
+
+
+// Schema là lược đồ database
+let user_schema = mongoose.Schema({
+
+    email: {
+        type : String,
+        default: "Chưa có thông tin"
+    },
+
+    username: {
+        type: String,
+        unique: true, 
+        require: true,
+    },
+    password:{
+        type: String,
+        require: true,
+    },
+    phone: {
+        type: String,
+    }
+})
+
+// Xác thực Unique (username là trường duy nhất)
+user_schema.plugin(uniqueValidator)
+
+exports.User = mongoose.model("User", user_schema);
+
