4015958cd122aae05dc5449bea06a77c05e35658
James1108Nguyen <71433464+James1108Nguyen@users.noreply.github.com>
2021-06-03 16:22:36 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -12,7 +12,6 @@ let user_schema = mongoose.Schema({
 
     username: {
         type: String,
-        unique: true, 
         require: true
     },
     password:{
@@ -41,7 +40,6 @@ let user_schema = mongoose.Schema({
 })
 
 // Xác thực Unique (username là trường duy nhất)
-user_schema.plugin(uniqueValidator)
 
 exports.User = mongoose.model("User", user_schema);
 
