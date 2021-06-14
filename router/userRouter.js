f1c7b580ae34c68773aeceb62390a7e0fba3dfba
James1108Nguyen <71433464+James1108Nguyen@users.noreply.github.com>
2021-06-15 02:25:24 +0700

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -53,7 +53,7 @@ router.post("/login", async function(req,res){
 //Change Password
 router.post("/changePass", async function(req,res){
 let user = await User.findOne({username : req.body.username})
-if (!bcrypt.compareSync(req.body.currentPassword , user.newPassword)){
+if (!bcrypt.compareSync(req.body.currentPassword , user.password)){
   return res.status(422).send("Rất tiếc, mật khẩu của bạn không đúng. Vui lòng kiểm tra lại mật khẩu.")
 }
 User.findOneAndUpdate({username : req.body.username},{password:bcrypt.hashSync(req.body.newPassword, salt)},{new : true},(error,data) => {
