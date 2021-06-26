785df888d8bd321c7eb2e67d5e1babb8c8852880
James1108Nguyen <71433464+James1108Nguyen@users.noreply.github.com>
2021-06-26 18:20:23 +0700

    Update foodRouter.js

--- router/foodRouter.js
+++ router/foodRouter.js
@@ -31,6 +31,7 @@ router.post('/foodtype', async (req, res) => {
     let newtype = foodType({
         type: req.body.type,
         typeName: req.body.typeName,
+        image: res.body.image,
     })
     newtype.save()
     .then((newtype)=> {
